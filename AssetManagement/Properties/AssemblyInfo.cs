4e5ae776f90fc1b8306f4ffc666aeb834f0844b6
Bhavya Soni <bhavya.soni@intimetec.com>
2025-04-12 12:18:59 +0530

    Updated

--- AssetManagement/Properties/AssemblyInfo.cs
+++ AssetManagement/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("5c605062-587a-40e8-8867-5090e3b9a4a6")]
+[assembly: Guid("24d1fd23-711d-4976-88d9-d37e52d7cfde")]
 
 // Version information for an assembly consists of the following four values:
 //
